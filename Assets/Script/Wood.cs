2e73d59d6ca93a23dbc2d81ea4e3c0f6e80e53b9
YUANXIUQU <s5624234@bournemouth.ac.uk>
2024-10-27 00:07:47 +0100

    add particle effect

--- Assets/Script/Wood.cs
+++ Assets/Script/Wood.cs
@@ -7,13 +7,20 @@ public class Wood : MonoBehaviour
     public float speed;
     public bool stopmove;
     public static bool createnext;
+
     public GameObject rock1;
     public GameObject rock2;
     public AudioSource wood_settled;
+    public ParticleSystem effect;
+
+    int num_stopmove;
+    int num_effect;
+
+
     // Update is called once per frame
     void Start()
     {
-        
+        num_stopmove = 0;
         stopmove = false;
         rock1.SetActive(false);
         rock2.SetActive(false);
@@ -27,12 +34,27 @@ public class Wood : MonoBehaviour
             rock1.SetActive(true);
             rock2.SetActive(true);
             wood_settled.Play();
+            num_stopmove++;
         }
+
         if(stopmove == false)
         {
             transform.Translate(Vector3.down * speed * speed * Time.deltaTime);
         }
+        if(num_stopmove == 1)
+        {
+            playeffect();
+        }
 
   
+    }
+
+    void playeffect()
+    {
+        if(num_effect == 0)
+        {
+            effect.Play();
+        }
+        num_effect++;
     }
 }
